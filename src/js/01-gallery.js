244a4fce77974995178745b92800377200badb7e
Daria Ośka-Ossowska <daria.oska@gmail.com>
2022-07-31 23:47:46 +0200

    Zadanie 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,7 +2,11 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-// console.log(galleryItems);
+console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const gallery = document.querySelector('.gallery');
 const items = [];
